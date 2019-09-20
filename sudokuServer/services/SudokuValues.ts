9b278ab2f6bfa3fd8737f41102c6b059c4c108e6
Sridhar Matta <mattasridhar19@gmail.com>
2019-09-20 02:33:34 -0400

    SolutionRefined for Sudoku

--- sudokuServer/services/SudokuValues.ts
+++ sudokuServer/services/SudokuValues.ts
@@ -9,9 +9,26 @@ export = class SudokuValues {
     private _randomGen; // array for storing the randomly generated numbers
     private _checkColArray; //for storing _board column entries for checking whether the value is contained or not
     private _partBoard: number[][] = new Array(); //for storing 3x3 _board entries
-    cnt:number =0;
+    tracker: number = 0;
 
+    /* private _sudokuBoard: number[][] = [
+        [0, 0, 0],
+        [0, 0, 0],//[0, 1, 0],
+        [0, 0, 0],//[0, 0, 2],
+    ]; */
     private _sudokuBoard: number[][] = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+
+    /* private _sudokuBoard: number[][] = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 1, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 2, 0, 0, 0, 0, 0, 0],
@@ -21,7 +38,7 @@ export = class SudokuValues {
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ];
+    ]; */
 
     /*
     _board.push([3,4,5]); ==> 0th row
@@ -37,15 +54,15 @@ export = class SudokuValues {
         this._value = value;
         this._board = this._sudokuBoard;
         //TODO: put a replace method here if we have row/col/value
-        console.log("SRI rowContains: " + this._rowContainsValue(2, 2));
+        /* console.log("SRI rowContains: " + this._rowContainsValue(2, 2));
         console.log("SRI columnContains: " + this._columnContainsValue(2, 2));
-        console.log("SRI isPotentialCandidate: " + this._isPotentialCandidate(2, 2, 22));
+        console.log("SRI isPotentialCandidate: " + this._isPotentialCandidate(2, 2, 22)); */
     }
 
     //returns true if 'toBeStored' is found in the specific row
     private _rowContainsValue(rowIndex: number, toBeStored: number) {
-        // console.log(this._board[rowIndex])
         if (this._board[rowIndex].indexOf(toBeStored) !== -1) {
+            //remove the value already stored in Board
             return true;
         }
         return false;
@@ -53,23 +70,40 @@ export = class SudokuValues {
 
     //returns true if 'toBeStored' is found in the specific column
     private _columnContainsValue(columnIndex: number, toBeStored: number) {
-        this._checkColArray = [];
-        for (var r = 0; r < this._BOARD_LENGTH; r++) {
-            if (this._board[r].length > 0) {
-                this._checkColArray.push(this._board[r][columnIndex]);
-            }
-        }
+        this._checkColArray = this._board.map(function (value, index) { return value[columnIndex]; });
         // console.log(this._checkColArray);
         if (this._checkColArray.indexOf(toBeStored) !== -1) {
             return true;//column lacks the 'toBeStored' Value
         }
-
         return false;//column has the value
-
     }
 
     //check for Each 9x9 matrix chunk of the board and return true if 'toBeStored' value is found
     private _matrixContainsValue(rowIndex: number, columnIndex: number, toBeStored: number) {
+        const minRow = rowIndex - (rowIndex % 3);
+        const minColumn = columnIndex - (columnIndex % 3);
+        let found: boolean = false;
+        let rowData: number[] = new Array();
+
+        // console.log("\ttoBeStores: " + toBeStored);
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                rowData.push(this._board[minRow + i][minColumn + j]);
+                // let colData = this._board.map(function (value, index) { return value[minColumn + i]; });
+
+            }
+            // console.log(rowData);
+            // console.log("\t" + colData);
+            if ((rowData.indexOf(toBeStored) !== -1)) {// || (colData.indexOf(toBeStored) !== -1)) {
+                found = true;//either the row or column of the matrix has value
+            }
+            rowData = [];
+        }
+
+
+        return found;
+    }
+    /* private _matrixContainsValue(rowIndex: number, columnIndex: number, toBeStored: number) {
         let matrixRow = rowIndex - (rowIndex % 3);
         let matrixColumn = columnIndex - (columnIndex % 3);
         let found: boolean = false;
@@ -94,40 +128,66 @@ export = class SudokuValues {
         }
         // console.log("FOUNDme?: " + found + "\n");
         return found;
-    }
+    } */
 
     //Verify if the 'toBeStored' value is a potential candidate or not for the specific cell
     private _isPotentialCandidate(rowIndex: number, columnIndex: number, toBeStored: number) {
-        return !this._rowContainsValue(rowIndex, toBeStored) && !this._columnContainsValue(columnIndex, toBeStored) && !this._matrixContainsValue(rowIndex, columnIndex, toBeStored);
+        let tmp = !this._matrixContainsValue(rowIndex, columnIndex, toBeStored);
+        // console.log(tmp);
+        return !this._rowContainsValue(rowIndex, toBeStored) && !this._columnContainsValue(columnIndex, toBeStored) && tmp;// && !this._matrixContainsValue(rowIndex, columnIndex, toBeStored);
     }
 
     //create the Sudoku board with potential values. 
     //making use of Backtracking Algorithm in a recursive manner. Reference: https://www.tutorialspoint.com/introduction-to-backtracking-algorithms
     private _populateBoardValues() {
+        loop1:
         for (let r = 0; r < this._BOARD_LENGTH; r++) {
+            this._randomGen = this.randomArrayGenerator();
+            // console.log("\nBORAD row BFR: " + this._board[r]);
+            this.tracker = 0;
+            loop2:
             for (let c = 0; c < this._BOARD_LENGTH; c++) {
+                console.log("BFR RANDOM_ARR: " + this._randomGen);
                 //replace all 'toBeReplaced' locations
-                if (this._board[r][c] === this._toBeReplaced) {
-                    //trying all potential values in our range
-                    for (let toBeStored = 1; toBeStored <= this._BOARD_LENGTH; toBeStored++) {
-                        // console.log("toBeStored: " + toBeStored);
-                        if (this._isPotentialCandidate(r, c, toBeStored)) {
-                            console.log("AM IN!!!!:   ");
-                            this._board[r][c] = toBeStored;
-                            //Backtracking
-                            if (this._populateBoardValues()) {
-                            console.log("TRUE IN!!!!:   " + this.cnt++);
-                                return true;
-                            } else {
-                                this._board[r][c] = this._toBeReplaced;
+                loop3:
+                while (this._randomGen.length !== 0) {
+
+                    if (this._board[r][c] !== this._toBeReplaced) {
+                        //remove the value already stored in Board
+                        let existValueIndex = this._randomGen.indexOf(this._board[r][c]);
+                        console.log("existValIn: " + existValueIndex)
+                        // if (existValueIndex > -1) {
+                        this._randomGen.splice(existValueIndex, 1);
+                        // }
+
+                        this.tracker = 0;
+                        console.log("BREAKING loop3 AS VALUE is FIXED: " + this._board[r][c]);
+                        //break loop3;//brk while and increement 'c' as value is already fixed
+                    }
+
+                    if (this._board[r][c] === this._toBeReplaced) {
+                        //trying all potential values in our range
+                        if (this._isPotentialCandidate(r, c, this._randomGen[this.tracker])) {
+                            console.log("\tPotentialCandidate tracker: " + this.tracker + " randGen[tracker]: " + this._randomGen[this.tracker]);
+                            this._board[r][c] = this._randomGen[this.tracker];
+                            this._randomGen.splice(this.tracker, 1);
+                            this.tracker = 0;
+                            break loop3;//brk while increement 'c' if match is found nd value is inserted
+                        } else {
+                            this.tracker++;
+                            // console.log("\tInc K: " + k + " C: " + c);
+                            if (this.tracker >= this._randomGen.length) {
+                                this.tracker = 0;
                             }
                         }
                     }
-                    return false;
                 }
+                console.log("AFTR RANDOM_ARR: " + this._randomGen);
+                // console.log("\trandomGen Index K: " + k + " Board Col Index: " + c);
+                console.log("BORAD row AFTR: " + this._board[r] + "\n-----------------------------------------------------\n");
             }
+            // console.log(this._board);
         }
-        return true;
     }
 
     //creates our sudoku board provided no cell was selected in UI
@@ -135,7 +195,7 @@ export = class SudokuValues {
         // console.log("SRI Initial Board: ");
         // console.log(this._board);
         this._populateBoardValues();
-        console.log("SRI Final Board:");
+        console.log("\n\nSRI Final Board:");
         console.log(this._board);
         this._board;
     }
@@ -191,13 +251,13 @@ export = class SudokuValues {
 
     //Generates an array with random arrangement of the numbers 1-9
     private randomArrayGenerator() {
-        const entries = [1, 2, 3, 4, 5, 6, 7, 8, 9];//[1, 2, 3, 4];//
+        const entries = [1, 2, 3, 4, 5, 6, 7, 8, 9];//[1, 2, 3, 4];//[1, 2, 3];//
         let randomArray = [];
         for (var tempArr = entries, i = tempArr.length; i--;) {
             var random = tempArr.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
             randomArray.push(random);
         }
-        console.log("\nSRI RANDOM ARRAY: " + randomArray);
+        // console.log("\nSRI RANDOM ARRAY: " + randomArray);
         return randomArray;
     }
 }
--- sudokuServer/transpiledJs/SudokuValues.js
+++ sudokuServer/transpiledJs/SudokuValues.js
@@ -1,5 +1,16 @@
 "use strict";
 module.exports = /** @class */ (function () {
+    /* private _sudokuBoard: number[][] = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 2, 0, 0, 0, 0, 0, 0],
+        [0, 0, 4, 3, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]; */
     /*
     _board.push([3,4,5]); ==> 0th row
     _board.push([6,7,8]); ==> 1st row
@@ -12,12 +23,17 @@ module.exports = /** @class */ (function () {
         this._toBeReplaced = 0; //value to be replaced in sudokuBoard
         this._BOARD_LENGTH = 9;
         this._partBoard = new Array(); //for storing 3x3 _board entries
-        this.cnt = 0;
+        this.tracker = 0;
+        /* private _sudokuBoard: number[][] = [
+            [0, 0, 0],
+            [0, 0, 0],//[0, 1, 0],
+            [0, 0, 0],//[0, 0, 2],
+        ]; */
         this._sudokuBoard = [
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 2, 0, 0, 0, 0, 0, 0],
-            [0, 0, 4, 3, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -29,26 +45,21 @@ module.exports = /** @class */ (function () {
         this._value = value;
         this._board = this._sudokuBoard;
         //TODO: put a replace method here if we have row/col/value
-        console.log("SRI rowContains: " + this._rowContainsValue(2, 2));
+        /* console.log("SRI rowContains: " + this._rowContainsValue(2, 2));
         console.log("SRI columnContains: " + this._columnContainsValue(2, 2));
-        console.log("SRI isPotentialCandidate: " + this._isPotentialCandidate(2, 2, 22));
+        console.log("SRI isPotentialCandidate: " + this._isPotentialCandidate(2, 2, 22)); */
     }
     //returns true if 'toBeStored' is found in the specific row
     SudokuValues.prototype._rowContainsValue = function (rowIndex, toBeStored) {
-        // console.log(this._board[rowIndex])
         if (this._board[rowIndex].indexOf(toBeStored) !== -1) {
+            //remove the value already stored in Board
             return true;
         }
         return false;
     };
     //returns true if 'toBeStored' is found in the specific column
     SudokuValues.prototype._columnContainsValue = function (columnIndex, toBeStored) {
-        this._checkColArray = [];
-        for (var r = 0; r < this._BOARD_LENGTH; r++) {
-            if (this._board[r].length > 0) {
-                this._checkColArray.push(this._board[r][columnIndex]);
-            }
-        }
+        this._checkColArray = this._board.map(function (value, index) { return value[columnIndex]; });
         // console.log(this._checkColArray);
         if (this._checkColArray.indexOf(toBeStored) !== -1) {
             return true; //column lacks the 'toBeStored' Value
@@ -57,11 +68,33 @@ module.exports = /** @class */ (function () {
     };
     //check for Each 9x9 matrix chunk of the board and return true if 'toBeStored' value is found
     SudokuValues.prototype._matrixContainsValue = function (rowIndex, columnIndex, toBeStored) {
-        var matrixRow = rowIndex - (rowIndex % 3);
-        var matrixColumn = columnIndex - (columnIndex % 3);
+        var minRow = rowIndex - (rowIndex % 3);
+        var minColumn = columnIndex - (columnIndex % 3);
         var found = false;
-        loop1: for (var r = matrixRow; (r < (matrixRow + 3) && r < this._BOARD_LENGTH); r++) {
-            for (var c = matrixColumn; (c < (columnIndex + 3) && c < this._BOARD_LENGTH); c++) {
+        var rowData = new Array();
+        // console.log("\ttoBeStores: " + toBeStored);
+        for (var i = 0; i < 3; i++) {
+            for (var j = 0; j < 3; j++) {
+                rowData.push(this._board[minRow + i][minColumn + j]);
+                // let colData = this._board.map(function (value, index) { return value[minColumn + i]; });
+            }
+            // console.log(rowData);
+            // console.log("\t" + colData);
+            if ((rowData.indexOf(toBeStored) !== -1)) { // || (colData.indexOf(toBeStored) !== -1)) {
+                found = true; //either the row or column of the matrix has value
+            }
+            rowData = [];
+        }
+        return found;
+    };
+    /* private _matrixContainsValue(rowIndex: number, columnIndex: number, toBeStored: number) {
+        let matrixRow = rowIndex - (rowIndex % 3);
+        let matrixColumn = columnIndex - (columnIndex % 3);
+        let found: boolean = false;
+
+        loop1:
+        for (let r = matrixRow; (r < (matrixRow + 3) && r < this._BOARD_LENGTH); r++) {
+            for (let c = matrixColumn; (c < (columnIndex + 3) && c < this._BOARD_LENGTH); c++) {
                 // console.log("\t[r][c]: [" + r + "][" + c + "]");
                 // console.log("\tthis._board[" + r + "][" + c + "]: " + this._board[r][c]);
                 // console.log("\tthis._board[r][c]: " + this._board[r][c]);
@@ -69,8 +102,7 @@ module.exports = /** @class */ (function () {
                 // console.log("\t(this._board[r][c] === toBeStored): " + (this._board[r][c] === toBeStored));
                 if (this._board[r][c] === 0) {
                     break;
-                }
-                else if (this._board[r][c] === toBeStored) {
+                } else if (this._board[r][c] === toBeStored) {
                     // console.log("\tthis._board[" + r + "][" + c + "]: " + this._board[r][c]);
                     // console.log("GOTCHA BREAK ME!!!!!!!!");
                     found = true;
@@ -80,46 +112,66 @@ module.exports = /** @class */ (function () {
         }
         // console.log("FOUNDme?: " + found + "\n");
         return found;
-    };
+    } */
     //Verify if the 'toBeStored' value is a potential candidate or not for the specific cell
     SudokuValues.prototype._isPotentialCandidate = function (rowIndex, columnIndex, toBeStored) {
-        return !this._rowContainsValue(rowIndex, toBeStored) && !this._columnContainsValue(columnIndex, toBeStored) && !this._matrixContainsValue(rowIndex, columnIndex, toBeStored);
+        var tmp = !this._matrixContainsValue(rowIndex, columnIndex, toBeStored);
+        // console.log(tmp);
+        return !this._rowContainsValue(rowIndex, toBeStored) && !this._columnContainsValue(columnIndex, toBeStored) && tmp; // && !this._matrixContainsValue(rowIndex, columnIndex, toBeStored);
     };
     //create the Sudoku board with potential values. 
     //making use of Backtracking Algorithm in a recursive manner. Reference: https://www.tutorialspoint.com/introduction-to-backtracking-algorithms
     SudokuValues.prototype._populateBoardValues = function () {
-        for (var r = 0; r < this._BOARD_LENGTH; r++) {
-            for (var c = 0; c < this._BOARD_LENGTH; c++) {
+        loop1: for (var r = 0; r < this._BOARD_LENGTH; r++) {
+            this._randomGen = this.randomArrayGenerator();
+            // console.log("\nBORAD row BFR: " + this._board[r]);
+            this.tracker = 0;
+            loop2: for (var c = 0; c < this._BOARD_LENGTH; c++) {
+                console.log("BFR RANDOM_ARR: " + this._randomGen);
                 //replace all 'toBeReplaced' locations
-                if (this._board[r][c] === this._toBeReplaced) {
-                    //trying all potential values in our range
-                    for (var toBeStored = 1; toBeStored <= this._BOARD_LENGTH; toBeStored++) {
-                        // console.log("toBeStored: " + toBeStored);
-                        if (this._isPotentialCandidate(r, c, toBeStored)) {
-                            console.log("AM IN!!!!:   ");
-                            this._board[r][c] = toBeStored;
-                            //Backtracking
-                            if (this._populateBoardValues()) {
-                                console.log("TRUE IN!!!!:   " + this.cnt++);
-                                return true;
-                            }
-                            else {
-                                this._board[r][c] = this._toBeReplaced;
+                loop3: while (this._randomGen.length !== 0) {
+                    if (this._board[r][c] !== this._toBeReplaced) {
+                        //remove the value already stored in Board
+                        var existValueIndex = this._randomGen.indexOf(this._board[r][c]);
+                        console.log("existValIn: " + existValueIndex);
+                        // if (existValueIndex > -1) {
+                        this._randomGen.splice(existValueIndex, 1);
+                        // }
+                        this.tracker = 0;
+                        console.log("BREAKING loop3 AS VALUE is FIXED: " + this._board[r][c]);
+                        //break loop3;//brk while and increement 'c' as value is already fixed
+                    }
+                    if (this._board[r][c] === this._toBeReplaced) {
+                        //trying all potential values in our range
+                        if (this._isPotentialCandidate(r, c, this._randomGen[this.tracker])) {
+                            console.log("\tPotentialCandidate tracker: " + this.tracker + " randGen[tracker]: " + this._randomGen[this.tracker]);
+                            this._board[r][c] = this._randomGen[this.tracker];
+                            this._randomGen.splice(this.tracker, 1);
+                            this.tracker = 0;
+                            break loop3; //brk while increement 'c' if match is found nd value is inserted
+                        }
+                        else {
+                            this.tracker++;
+                            // console.log("\tInc K: " + k + " C: " + c);
+                            if (this.tracker >= this._randomGen.length) {
+                                this.tracker = 0;
                             }
                         }
                     }
-                    return false;
                 }
+                console.log("AFTR RANDOM_ARR: " + this._randomGen);
+                // console.log("\trandomGen Index K: " + k + " Board Col Index: " + c);
+                console.log("BORAD row AFTR: " + this._board[r] + "\n-----------------------------------------------------\n");
             }
+            // console.log(this._board);
         }
-        return true;
     };
     //creates our sudoku board provided no cell was selected in UI
     SudokuValues.prototype.createBoardValues = function () {
         // console.log("SRI Initial Board: ");
         // console.log(this._board);
         this._populateBoardValues();
-        console.log("SRI Final Board:");
+        console.log("\n\nSRI Final Board:");
         console.log(this._board);
         this._board;
     };
@@ -172,13 +224,13 @@ module.exports = /** @class */ (function () {
     }*/
     //Generates an array with random arrangement of the numbers 1-9
     SudokuValues.prototype.randomArrayGenerator = function () {
-        var entries = [1, 2, 3, 4, 5, 6, 7, 8, 9]; //[1, 2, 3, 4];//
+        var entries = [1, 2, 3, 4, 5, 6, 7, 8, 9]; //[1, 2, 3, 4];//[1, 2, 3];//
         var randomArray = [];
         for (var tempArr = entries, i = tempArr.length; i--;) {
             var random = tempArr.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
             randomArray.push(random);
         }
-        console.log("\nSRI RANDOM ARRAY: " + randomArray);
+        // console.log("\nSRI RANDOM ARRAY: " + randomArray);
         return randomArray;
     };
     return SudokuValues;
